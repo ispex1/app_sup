1528b6780e8ebe68f46bcd030e0516e0ffbf302f
aldeobacchus <gabord35@gmail.com>
2023-12-19 15:30:23 +0100

    equities commit

--- equities.py
+++ equities.py
@@ -37,7 +37,7 @@ def equities (X_path, y_path, sex_path, model, n):
     X_train, X_test, y_train, y_test, sex_train, sex_test = \
         train_test_split(X_used, y, sex, test_size=0.2, random_state=1)
 
-    print("\n========", model_name, "========")
+    print("\n#############################", model_name, "#############################")
     
     model.fit(X_train, y_train)
 
@@ -51,7 +51,7 @@ def equities (X_path, y_path, sex_path, model, n):
     X_female_test = [X_test[i] for i in range(X_test.shape[0]) if sex_test[i][0] == 2]
     y_female_test = [y_test['PINCP'].iat[i] for i in range(len(y_test)) if sex_test[i][0]== 2]
 
-    
+
 
     print("\n==== Quality of the model ====")
 
@@ -72,17 +72,46 @@ def equities (X_path, y_path, sex_path, model, n):
     confMatrix_female = confusion_matrix(y_female_test, y_female_pred)
     print("Confusion matrix for female: \n\n", confMatrix_female, "\n")
 
+    #statistical parity
+    print("\n==== Statistical parity ====")
+    print("comparaisons des taux de prédictions positives")
+
+    parity_male = np.sum(confMatrix_male[:,0])/np.sum(confMatrix_male)
+    print("statistical parity for male confusion matrix :", parity_male)
+
+    parity_female = np.sum(confMatrix_female[:,0])/np.sum(confMatrix_female)
+    print("statistical parity for female confusion matrix :", parity_female)
+
+    #equal opportunity
+    print("\n==== Equal opportunity ====")
+    print("comparer les taux de vrais positifs")
+
+    opportunity_male = confMatrix_male[0,0] / np.sum(confMatrix_male[0,:])
+    print("equal opportunity for male : ", opportunity_male)
+
+    opportunity_female = confMatrix_female[0,0] / np.sum(confMatrix_female[0,:])
+    print("equal opportunity for female : ", opportunity_female)
+
+    #predictive equality
+    print("\n==== Predictive equality ====")
+    print("comparer les taux de faux positifs")
+
+    pe_male= confMatrix_male[1,0] / np.sum(confMatrix_male[1,:])
+    print("predictive equality for male : ", pe_male)
+    
+    pe_female= confMatrix_female[1,0] / np.sum(confMatrix_female[1,:])
+    print("predictive equality for female : ", pe_female)
+
 models = {"SVM": sklearn.svm.SVC(C=1000, kernel="rbf", probability=True),
-          "Random Forest": sklearn.ensemble.RandomForestClassifier(max_depth=10, n_estimators=1000),
-          "Ada Boost": sklearn.ensemble.AdaBoostClassifier(learning_rate=1, n_estimators=1000),
-          "Gradient Boosting": sklearn.ensemble.GradientBoostingClassifier(learning_rate=1, max_depth=10,  n_estimators=100)}
+        "Random Forest": sklearn.ensemble.RandomForestClassifier(max_depth=10, n_estimators=1000),
+        "Gradient Boosting": sklearn.ensemble.GradientBoostingClassifier(learning_rate=1, max_depth=10,  n_estimators=100),
+        "Ada Boost": sklearn.ensemble.AdaBoostClassifier(learning_rate=1, n_estimators=1000)}
 
-
-
-
-X_path = '~/Bureau/5A/app-sup/data/acsincome_ca_features.csv'
-y_path = '~/Bureau/5A/app-sup/data/acsincome_ca_labels.csv'
-sex_path = '~/Bureau/5A/app-sup/data/acsincome_ca_group_Sex.csv'
+#pour tester avec le jeu de donnée sans la feature sex :
+#X_path = './data/acsincome_ca_features_without_sex.csv'
+X_path = './data/acsincome_ca_features.csv'
+y_path = './data/acsincome_ca_labels.csv'
+sex_path = './data/acsincome_ca_group_Sex.csv'
 
 for m, (name, model) in enumerate(models.items()):
     equities(X_path, y_path, sex_path, m, 0.05)
